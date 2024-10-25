0a634d84cefda7846c554c1e65554e9202ff6415
Diana Zatvarska <dizatvarska@gmail.com>
2024-10-24 18:23:20 -0700

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,58 +1,3 @@
-error = {
-    "code": "E501",
-    "filename": "./source_code_2.py",
-    "line_number": 18,
-    "column_number": 80,
-    "text": "line too long (99 > 79 characters)",
-    "physical_line": '    return f"I like to filter, rounding, doubling, '
-    "store and decorate numbers: {', '.join(items)}!\"",
-    }
-
-errors = [
-    {
-        "code": "E501",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 80,
-        "text": "line too long (99 > 79 characters)",
-        "physical_line": '    return f"I like to filter, rounding, doubling, ' 
-        "store and decorate numbers: {', '.join(items)}!\"",
-    },
-    {
-        "code": "W292",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 100,
-        "text": "no newline at end of file",
-        "physical_line": '    return f"I like to filter, rounding, doubling, '
-        "store and decorate numbers: {', '.join(items)}!\"",
-    },
-]
-report_file = {
-    "./test_source_code_2.py": [],
-    "./source_code_2.py":
-        [
-            {
-                "code": "E501",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 80,
-                "text": "line too long (99 > 79 characters)",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-            {
-                "code": "W292",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 100,
-                "text": "no newline at end of file",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-        ]
-}
-
 
 def format_linter_error(error: dict) -> dict:
     return {
@@ -64,28 +9,22 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-format_linter_error = format_linter_error(error=error)
+# format_linter_error(error=error)
 
 
-def format_single_linter_file(file_path: str, errors: list, error: dict) -> dict:
+def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
     "errors":
         [
-            format_linter_error,
-            {
-                "line": errors[1]["line_number"],
-                "column": errors[1]["column_number"],
-                "message": errors[1]["text"],
-                "name": errors[1]["code"],
-                "source": "flake8"
-            }
+            format_linter_error(error=errors[0]),
+            format_linter_error(error=errors[1])
         ],
     "path": file_path,
-    "status": "passed"
+    "status": "failed"
 }
 
 
-format_single = format_single_linter_file(file_path="./source_code_2.py", errors=errors, error=error)
+# format_single = format_single_linter_file(file_path="./source_code_2.py", errors=errors)
 
 
 def format_linter_report(linter_report: dict) -> list:
@@ -93,9 +32,9 @@ def format_linter_report(linter_report: dict) -> list:
     {
         "errors": report_file["./test_source_code_2.py"],
         "path": "./test_source_code_2.py",
-        "status": "passed"
-    }, format_single
+        "status": "failed"
+    }, format_single_linter_file(file_path="./source_code_2.py", errors=errors)
     ]
 
 
-format_linter_report(linter_report=report_file)
+# format_linter_report(linter_report=report_file)
